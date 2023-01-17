35af6854572d6b0172d5e04838112babe9af589c
timmyichen <timmy.i.chen@gmail.com>
2023-01-16 16:34:49 -0800

    add some basic rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,17 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   root: true,
   env: {
-    "node": true,
+    'node': true,
   },
   rules: {
-    semi: ['warn', 'always'],
-  }
+    semi: ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'no-extra-parens': 'error',
+    indent: ['error', 2],
+    'comma-dangle': ['error', 'always-multiline'],
+    quotes: ['error', 'single'],
+    'max-len': ['warn', 120],
+    'prefer-const': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 };
