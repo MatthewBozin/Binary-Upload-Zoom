5fa82a720204c534825e31da7c986cb70acd0e22
timmyichen <timmy.i.chen@gmail.com>
2023-01-20 19:30:00 -0800

    better URL building for discord login

--- server/api/page/page.controller.ts
+++ server/api/page/page.controller.ts
@@ -2,6 +2,11 @@ import { RequestHandler } from 'express';
 
 export const login: RequestHandler = (req, res) => {
   const redirectUri = 'http://localhost:2121/auth-redirect';
-  const url = `https://discord.com/oauth2/authorize?response_type=code&redirect_uri=${redirectUri}&scope=identify%20guilds&client_id=${process.env.DISCORD_CLIENT_ID}`;
+  const params = new URLSearchParams();
+  params.append('response_type', 'code');
+  params.append('redirect_uri', redirectUri);
+  params.append('scope', 'identify guilds');
+  params.append('client_id', process.env.DISCORD_CLIENT_ID);
+  const url = 'https://discord.com/oauth2/authorize?' + params.toString();
   res.redirect(url);
 };
