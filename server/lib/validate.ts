aa03a2ad0d55f8360320e6a6bbe5f38e22e0ce75
timmyichen <timmy.i.chen@gmail.com>
2023-01-26 18:03:23 -0800

    allow in tests

--- server/lib/validate.ts
+++ server/lib/validate.ts
@@ -1,4 +1,9 @@
 export function validateEnv(keys: string[]) {
+  // we may be missing env vars during tests, which is fine
+  if (process.env.NODE_ENV === 'test') {
+    return;
+  }
+
   const missing = keys.filter(key => !process.env[key]);
 
   if (missing.length) {
